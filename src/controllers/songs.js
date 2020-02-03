d1782da2059341c97e1facca721da7c93975db3f
Rui Correia <rmgc31@gmail.com>
2020-02-03 14:50:30 +0000

    songs can now be created

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -1,3 +1,30 @@
-const mongoose = require('mongoose');
+const Song = require('../models/song');
+const Album = require('../models/album');
+const Artist = require('../models/artist');
 
-const songSchema = new mongoose.Schema({});
+exports.create = (req, res) => {
+  const { albumId } = req.params;
+  const { artistId } = req.body;
+
+  Album.findById(albumId, (err, album) => {
+    if (!album) {
+      res.status(404).json({ error: 'The album could not be found.' });
+    } else {
+      Artist.findById(artistId, (err, artist) => {
+        if (!artist) {
+          res.status(404).json({ error: 'The artist could not be found.' });
+        } else {
+          const song = new Song({
+            name: req.body.name,
+            artist,
+            album,
+          });
+
+          song.save().then(() => {
+            res.status(200).json(song);
+          });
+        }
+      });
+    }
+  });
+};
